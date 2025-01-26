7b5eb5acd5a387f487a73b806fc2ca2c3698f623
RaunakDevHub <raunakshrivastava2@gmail.com>
2025-01-26 18:22:45 +0530

    weather

--- weather/app.js
+++ weather/app.js
@@ -4,9 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
+app.use('/', indexRouter);
 var app = express();
+
+router.get('/', function(req, res) {
+  res.render('index', { weather: null, err: null });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
